7a5be6dbc37bf6e4e3b3ff05e082ae1a800a073a
Paluru Abhign Sai <sai@ekstep.org>
2023-01-16 15:06:16 +0530

    Revert "Added diksha data products from other sunbird BB and custom scripts."

--- adhoc-jobs/python-scripts/src/main/python/services/user.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("read").getOrCreate()
-
-df = spark.read.csv("/home/raja/Desktop/Ekstep/cassandra/user/sunbird.user_202211020720.csv", header=True)
-
-df_filter = df.filter(df['profileusertype'].isNull()).select('userid', 'usertype', 'createddate')
-
-df_filter.repartition(1).write.mode('overwrite').format('csv').option('header', True).save("/home/raja/Desktop/Ekstep/cassandra/user/output")
--- adhoc-jobs/python-scripts/src/main/python/services/user_state and district.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df_pandas = pd.read_csv("/home/raja/Downloads/wrong_mapped_device_id's - wrong_mapped_device_id's (1).csv")
-L = df_pandas['device_id'].tolist()
-print(type(L))
